62ab1b7ff5df8a9fd14c460da1f25265935e3556
matteo-visum <matteo@visumdigital.com>
2024-09-17 10:28:02 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MechSoft.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
