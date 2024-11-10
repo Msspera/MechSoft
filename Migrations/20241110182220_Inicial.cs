737a796942d15d4705b80d5a955b685e888d3d95
felipe-totvs <131923684+felipe-totvs@users.noreply.github.com>
2024-11-10 15:23:49 -0300

    MIGRATIONS

---
+++ Migrations/20241110182220_Inicial.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MechSoft.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Carros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Modelo = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Marca = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Placa = table.Column<string>(type: "nvarchar(7)", maxLength: 7, nullable: false),
+                    Cor = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Quilometragem = table.Column<int>(type: "int", nullable: false),
+                    UltimaRevisao = table.Column<int>(type: "int", nullable: false),
+                    PeriodicidadeRevisao = table.Column<int>(type: "int", nullable: false),
+                    UltimaTrocaOleo = table.Column<int>(type: "int", nullable: false),
+                    PeriodicidadeTrocaOleo = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carros", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(90)", maxLength: 90, nullable: false),
+                    Celular = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(120)", maxLength: 120, nullable: false),
+                    Endereco = table.Column<string>(type: "nvarchar(120)", maxLength: 120, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Equipamentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Estado = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    UltimaRevisao = table.Column<DateOnly>(type: "date", nullable: false),
+                    PeriodicidadeRevisao = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Equipamentos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Financeiro",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DataHora = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Total_cx = table.Column<float>(type: "real", nullable: false),
+                    Acao_finan = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Financeiro", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pecas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Fornecedor = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Estoque = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pecas", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Carros");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Equipamentos");
+
+            migrationBuilder.DropTable(
+                name: "Financeiro");
+
+            migrationBuilder.DropTable(
+                name: "Pecas");
+        }
+    }
+}
